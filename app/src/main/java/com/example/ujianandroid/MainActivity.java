1ad0ea50b61d94eee639c563f5b6c0b8bf0ead4c
Acer Travelmate <hadyanilhamwijaya01@gmail.com>
2023-05-10 19:57:45 +0700

    nama lengkap sudah bisa diloop

--- app/src/main/java/com/example/ujianandroid/MainActivity.java
+++ app/src/main/java/com/example/ujianandroid/MainActivity.java
@@ -32,18 +32,19 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 String isian_nama_depan = edNamaDepan.getText().toString();
                 String isian_nama_belakang = edNamaBelakang.getText().toString();
-                String isian_umur = edUmur.getText().toString();
+                Integer isian_umur = Integer.valueOf(edUmur.getText().toString());
 
                 if(isian_nama_depan.isEmpty()){
                     Toast.makeText(getApplicationContext(), "Isian masih kosong", Toast.LENGTH_SHORT).show();
                 }else{
-                    String nama_lengkap = isian_nama_depan.concat(" ").concat(isian_nama_belakang).concat(" ").concat(isian_umur);
+                    String nama_lengkap = isian_nama_depan.concat(" ").concat(isian_nama_belakang);
                     daftar_nama.clear();
-                    daftar_nama.add(nama_lengkap);
+
+                    for (int i = 0; i < isian_umur; i++) {
+                        daftar_nama.add(nama_lengkap);
+                    }
                     edNamaDepan.setText("");
                     edNamaBelakang.setText("");
-                    edUmur.setText("");
-
                     intent_list.putStringArrayListExtra("daftar_nama", daftar_nama);
                     startActivity(intent_list);
 
